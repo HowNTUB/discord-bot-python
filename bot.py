058107e3a4f81ac9f9ddf7dcb030965bbc89cdc4
DESKTOP-QS0J25F\User <10456003@ntub.edu.tw>
2021-09-21 18:53:11 +0800

    v1.0

---
+++ bot.py
@@ -0,0 +1,84 @@
+# bot.py
+import os
+import random
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+import youtube_dl
+import os
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+
+intents = discord.Intents().all()
+
+client = commands.Bot(command_prefix=".", intents=intents)
+
+@client.event
+async def on_ready():
+    for guild in client.guilds:
+        if guild.name == GUILD:
+            break
+    print(
+        f'{client.user} is connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})'
+    )
+
+@client.command(pass_context = True)
+async def dice(ctx):
+    await ctx.send(f'骰子丟到{random.randint(1, 6)}')
+
+@client.command(pass_context = True)
+async def play(ctx, url : str):
+    song_there = os.path.isfile("song.mp3")
+    try:
+        if song_there:
+            os.remove("song.mp3")
+    except PermissionError:
+        await ctx.send("等待目前的音樂播放結束")
+
+    voiceChannel = discord.utils.get(ctx.guild.voice_channels, name='How')
+    await voiceChannel.connect()
+    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
+    
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'postprocessors':[{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        }],
+    }
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
+    for file in os.listdir("./"):
+        if file.endswith(".mp3"):
+            os.rename(file, "song.mp3")
+    voice.play(discord.FFmpegPCMAudio("song.mp3"))
+
+@client.command(pass_context = True)
+async def leave(ctx):
+    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
+    if voice.is_connected():
+        await voice.disconnect()
+    else:
+        await ctx.send("我目前沒有在語音頻道裡！")
+
+@client.command(pass_context = True)
+async def pause(ctx):
+    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
+    if voice.is_playing():
+        voice.pause()
+    else:
+        await ctx.send("目前沒有在播放音樂！")
+
+@client.command(pass_context = True)
+async def resume(ctx):
+    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
+    if voice.is_paused():
+        voice.resume()
+    else:
+        await ctx.send("目前音樂在播放中哦")
+
+client.run(TOKEN)
